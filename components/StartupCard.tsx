97ec5b525fea6e85cb8ffea99a7100fa36fcd203
Georgios Georgiou <georgios.georgiou@newcrosshealthcare.com>
2024-11-13 23:52:12 +0200

    init incremental ppr

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -61,7 +61,7 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
           <p className="text-15-medium">{category}</p>
         </Link>
         <Button className="startup-card_btn" asChild>
-          <Link href={`/?startup/${_id}`}>Details</Link>
+          <Link href={`/startup/${_id}`}>Details</Link>
         </Button>
       </div>
     </li>
